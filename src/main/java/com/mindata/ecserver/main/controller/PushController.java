aa3cf82e143fbd4da3d87cb915bddd5ae1d42262
wuwf <272551766@qq.com>
2017-11-03 18:34:27 +0800

    统计功能未完成，测试推送5千条

--- src/main/java/com/mindata/ecserver/main/controller/PushController.java
+++ src/main/java/com/mindata/ecserver/main/controller/PushController.java
@@ -12,6 +12,8 @@ import com.mindata.ecserver.main.requestbody.PushBody;
 import com.mindata.ecserver.main.service.PushService;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,7 +36,7 @@ public class PushController {
     private PtUserPushThresholdManager ptUserPushThresholdManager;
 
     private static final int MAX_SIZE = 50;
-
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
     /**
      * 推送指定的id集合到ec
      *
@@ -60,5 +63,56 @@ public class PushController {
             return ResultGenerator.genFailResult(ResultCode.PUSH_COUNT_BEYOND_TODAY_LIMIT, "已超出今日最大限制");
         }
         return ResultGenerator.genSuccessResult(pushService.push(pushBody));
+    }
+
+    @CheckEcAnnotation
+    @RequestMapping("/push")
+    public Object push() throws IOException {
+        int beginId = 218302;
+        int endId = 223301;
+
+        long optUserId = 4;//侯学明
+        long qizhi = 46;
+        long zhao = 47;
+
+        int count = 0;
+        List<Integer> ids = new ArrayList<>();
+
+        PushBody pushBody = new PushBody();
+        pushBody.setOptUserId(optUserId);
+        pushBody.setFollowUserId(qizhi);
+        for (int i = beginId; i <= 220802; i++) {
+            ids.add(i);
+            if (count != 0 && count % 50 == 0) {
+                pushBody.setIds(ids);
+                pushService.push(pushBody);
+                ids.clear();
+                logger.info("已推送50个");
+            }
+            count++;
+        }
+        if (ids.size() > 0) {
+            pushService.push(pushBody);
+            logger.info("给qizhi的推送完毕");
+        }
+
+        ids.clear();
+        count = 0;
+        pushBody.setFollowUserId(zhao);
+        for (int i = 220803; i <= endId; i++) {
+            ids.add(i);
+            if (count != 0 && count % 50 == 0) {
+                pushBody.setIds(ids);
+                pushService.push(pushBody);
+                ids.clear();
+                logger.info("已推送50个");
+            }
+            count++;
+        }
+        if (ids.size() > 0) {
+            pushService.push(pushBody);
+            logger.info("给qizhi的推送完毕");
+        }
+        return "success";
     }
 }

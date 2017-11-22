e83190901f75b12140efd0c5fa514337435c0777
wuwf <272551766@qq.com>
2017-11-22 17:00:17 +0800

    获取用户角色名

--- src/main/java/com/mindata/ecserver/main/manager/PtRoleManager.java
+++ src/main/java/com/mindata/ecserver/main/manager/PtRoleManager.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -29,6 +30,8 @@ public class PtRoleManager {
     private UserRoleCache userRoleCache;
     @Resource
     private PtMenuRoleRepository ptMenuRoleRepository;
+    @Resource
+    private PtUserManager ptUserManager;
 
     /**
      * 查询用户的所有role
@@ -45,6 +48,29 @@ public class PtRoleManager {
 
     public PtRole findByRoleId(Long roleId) {
         return ptRoleRepository.findOne(roleId);
+    }
+
+    /**
+     * 判断用户的角色
+     *
+     * @param ptUser
+     *         用户
+     * @return 角色名
+     */
+    public String getRoleStr(PtUser ptUser) {
+        //判断用户角色
+        List<PtRole> roles = findRolesByUser(ptUser);
+        Set<String> roleNames = roles.stream().map(PtRole::getName).collect(Collectors.toSet());
+        if (roleNames.contains(Constant.ROLE_ADMIN)) {
+            return Constant.ROLE_ADMIN;
+        }
+        if (roleNames.contains(Constant.ROLE_MANAGER)) {
+            return Constant.ROLE_MANAGER;
+        }
+        if (roleNames.contains(Constant.ROLE_LEADER)) {
+            return Constant.ROLE_LEADER;
+        }
+        return Constant.ROLE_USER;
     }
 
     /**
@@ -77,15 +103,16 @@ public class PtRoleManager {
         return menuRoles.stream().map(PtMenuRole::getRoleId).map(this::findByRoleId).collect(Collectors.toList());
     }
 
+    /**
+     * 判断是否是管理员
+     *
+     * @param userId
+     *         用户id
+     * @return 是否是管理员
+     */
     public boolean isManager(Long userId) {
-        //判断用户角色
-        List<PtRole> roles = findByUserId(userId);
-        for (PtRole ptRole : roles) {
-            if (Constant.ROLE_MANAGER.equals(ptRole.getName())) {
-                return true;
-            }
-        }
-        return false;
+        PtUser ptUser = ptUserManager.findByUserId(userId);
+        return Constant.ROLE_MANAGER.equals(getRoleStr(ptUser));
     }
 
     public Long findIdByName(String name) {

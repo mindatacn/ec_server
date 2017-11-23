e178071245096defb7cedcd97c398fe4ef0d7ed9
wuwf <272551766@qq.com>
2017-11-23 09:28:31 +0800

    推送失败历史

--- src/main/java/com/mindata/ecserver/main/service/PushFailResultService.java
+++ src/main/java/com/mindata/ecserver/main/service/PushFailResultService.java
@@ -10,7 +10,6 @@ import com.mindata.ecserver.main.manager.PtPushResultManager;
 import com.mindata.ecserver.main.manager.PtUserManager;
 import com.mindata.ecserver.main.model.primary.EcContactEntity;
 import com.mindata.ecserver.main.model.secondary.PtPushFailureResult;
-import com.mindata.ecserver.main.model.secondary.PtUser;
 import com.mindata.ecserver.main.requestbody.PushFailRequestBody;
 import com.mindata.ecserver.main.vo.PushFailResultVO;
 import com.xiaoleilu.hutool.date.DateUtil;
@@ -25,9 +24,6 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.mindata.ecserver.global.constant.Constant.STATE_NORMAL;
 
 /**
  * @author hanliqiang wrote on 2017/11/16
@@ -56,17 +52,17 @@ public class PushFailResultService {
         if (StrUtil.isNotEmpty(pushFailRequestBody.getCompanyName())) {
             criteria.add(Restrictions.like("companyName", pushFailRequestBody.getCompanyName(), true));
         }
-        //推送团队模糊查询
-        if (pushFailRequestBody.getDeptId() != null) {
-            List<PtUser> userList = ptUserManager.findByDeptIdAndState(pushFailRequestBody.getDeptId(), STATE_NORMAL);
-            List<Long> list = userList.stream().map(PtUser::getId).collect(Collectors.toList());
-            criteria.add(Restrictions.in("followUserId", list, true));
-        }
-        //推送人模糊查询
-        if (pushFailRequestBody.getUserId() != null) {
-            PtUser ptUser = ptUserManager.findByUserId(pushFailRequestBody.getUserId());
-            criteria.add(Restrictions.eq("followUserId", ptUser.getId(), true));
-        }
+        ////推送团队模糊查询
+        //if (pushFailRequestBody.getDeptId() != null) {
+        //    List<PtUser> userList = ptUserManager.findByDeptIdAndState(pushFailRequestBody.getDeptId(), STATE_NORMAL);
+        //    List<Long> list = userList.stream().map(PtUser::getId).collect(Collectors.toList());
+        //    criteria.add(Restrictions.in("followUserId", list, true));
+        //}
+        ////推送人模糊查询
+        //if (pushFailRequestBody.getUserId() != null) {
+        //    PtUser ptUser = ptUserManager.findByUserId(pushFailRequestBody.getUserId());
+        //    criteria.add(Restrictions.eq("followUserId", ptUser.getId(), true));
+        //}
         //开始时间
         if (StrUtil.isNotEmpty(pushFailRequestBody.getBeginTime())) {
             Date date = DateUtil.beginOfDay(DateUtil.parseDate(pushFailRequestBody.getBeginTime()));

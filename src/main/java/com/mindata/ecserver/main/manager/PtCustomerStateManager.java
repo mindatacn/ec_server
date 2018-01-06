2fe5d0a2f0c12ce8ac1c4613e46d868ce59529fc
wuwf <272551766@qq.com>
2018-01-06 10:10:28 +0800

    意向率处理，不再使用entitymanager

--- src/main/java/com/mindata/ecserver/main/manager/PtCustomerStateManager.java
+++ src/main/java/com/mindata/ecserver/main/manager/PtCustomerStateManager.java
@@ -1,49 +1,39 @@
 package com.mindata.ecserver.main.manager;
 
-import com.mindata.ecserver.global.specify.Criteria;
-import com.mindata.ecserver.global.specify.MySelector;
-import com.mindata.ecserver.global.specify.Restrictions;
-import com.mindata.ecserver.main.model.secondary.PtCustomerState;
-import com.mindata.ecserver.util.CommonUtil;
+import com.mindata.ecserver.main.repository.secondary.PtCustomerStateRepository;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author wuweifeng wrote on 2017/12/28.
  */
 @Component
 public class PtCustomerStateManager {
-    @Resource(name = "entityManagerSecondary")
-    private EntityManager entityManager;
+    @Resource
+    private PtCustomerStateRepository ptCustomerStateRepository;
 
-    private Criteria<PtCustomerState> buildCriteria(Integer sourceFrom, Date begin, Date end) {
-        Criteria<PtCustomerState> criteria = new Criteria<>();
-        criteria.add(Restrictions.eq("oldData", false, true));
-        criteria.add(Restrictions.gt("operateTime", begin, true));
-        criteria.add(Restrictions.lt("operateTime", end, true));
-        if (sourceFrom != 0) {
-            criteria.add(Restrictions.eq("sourceFrom", sourceFrom, true));
+    private Integer count(Integer sourceFrom, Integer saleState, String operateType, Date begin, Date end) {
+        List<Integer> sourceFromList, saleStateList;
+        if (sourceFrom == null || sourceFrom == 0) {
+            sourceFromList = Arrays.asList(1, 2, 3);
+        } else {
+            sourceFromList = Arrays.asList(sourceFrom);
         }
-        return criteria;
-    }
-
-    private MySelector buildSelections(String fieldName) {
-        return Restrictions.countDistinct(fieldName);
-    }
-
-    private CriteriaQuery buildQuery(Criteria<PtCustomerState> criteria) {
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery criteriaQuery = builder.createQuery();
-        Root<PtCustomerState> root = criteriaQuery.from(PtCustomerState.class);
-        criteriaQuery.select(buildSelections("crmId").getSelection(root, builder));
-        criteriaQuery.where(criteria.toPredicate(root, criteriaQuery, builder));
-        return criteriaQuery;
+        if (saleState == null) {
+            saleStateList = Arrays.asList(0, 1, 2);
+        } else {
+            saleStateList = Arrays.asList(saleState);
+        }
+        if (operateType != null) {
+            return ptCustomerStateRepository.countDistinctByOperateType(begin, end, sourceFromList, operateType);
+        } else {
+            return ptCustomerStateRepository.countDistinctAndOperateTimeBetween(begin, end, sourceFromList,
+                    saleStateList);
+        }
     }
 
     /**
@@ -56,9 +46,7 @@ public class PtCustomerStateManager {
      * @return 数量
      */
     public Integer countDistinctCustomerBetween(Integer sourceFrom, Date begin, Date end) {
-        Criteria<PtCustomerState> criteria = buildCriteria(sourceFrom, begin, end);
-        Object distinctCount = entityManager.createQuery(buildQuery(criteria)).getSingleResult();
-        return CommonUtil.parseObject(distinctCount).intValue();
+        return count(sourceFrom, null, null, begin, end);
     }
 
     /**
@@ -71,10 +59,7 @@ public class PtCustomerStateManager {
      * @return 数量
      */
     public Integer countByOperateTypeAndTimeBetween(String type, Integer sourceFrom, Date begin, Date end) {
-        Criteria<PtCustomerState> criteria = buildCriteria(sourceFrom, begin, end);
-        criteria.add(Restrictions.eq("operateType", type, true));
-        Object distinctCount = entityManager.createQuery(buildQuery(criteria)).getSingleResult();
-        return CommonUtil.parseObject(distinctCount).intValue();
+        return count(sourceFrom, null, type, begin, end);
     }
 
     /**
@@ -87,10 +72,7 @@ public class PtCustomerStateManager {
      * @return 数量
      */
     public Integer countByIntentedAndTimeBetween(Integer sourceFrom, Integer saleState, Date begin, Date end) {
-        Criteria<PtCustomerState> criteria = buildCriteria(sourceFrom, begin, end);
-        criteria.add(Restrictions.eq("saleState", saleState, true));
-        Object distinctCount = entityManager.createQuery(buildQuery(criteria)).getSingleResult();
-        return CommonUtil.parseObject(distinctCount).intValue();
+        return count(sourceFrom, saleState, null, begin, end);
     }
 
 }
--- src/main/java/com/mindata/ecserver/main/repository/secondary/PtCustomerStateRepository.java
+++ src/main/java/com/mindata/ecserver/main/repository/secondary/PtCustomerStateRepository.java
@@ -3,10 +3,22 @@ package com.mindata.ecserver.main.repository.secondary;
 import com.mindata.ecserver.main.model.secondary.PtCustomerState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author wuweifeng wrote on 2017/10/25.
  */
 public interface PtCustomerStateRepository extends JpaRepository<PtCustomerState, Long>,
         JpaSpecificationExecutor<PtCustomerState> {
+
+    @Query("select count(distinct crmId) from PtCustomerState where operateTime > ?1 and operateTime < ?2" +
+            " and oldData = 0 and sourceFrom in ?3 and saleState in ?4")
+    Integer countDistinctAndOperateTimeBetween(Date begin, Date end, List<Integer> sourceFrom, List<Integer> saleState);
+
+    @Query("select count(distinct crmId) from PtCustomerState where operateTime > ?1 and operateTime < ?2" +
+            " and oldData = 0 and sourceFrom in ?3 and operateType = ?4")
+    Integer countDistinctByOperateType(Date begin, Date end, List<Integer> sourceFrom, String operateType);
 }

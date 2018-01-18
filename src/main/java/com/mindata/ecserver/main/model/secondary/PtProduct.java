4bb37884c2e610cd26f44258888d1021573f926b
wuwf <272551766@qq.com>
2018-01-18 15:46:34 +0800

    产品管理

---
+++ src/main/java/com/mindata/ecserver/main/model/secondary/PtProduct.java
@@ -0,0 +1,63 @@
+package com.mindata.ecserver.main.model.secondary;
+
+import com.mindata.ecserver.main.model.base.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+/**
+ * 不同平台的产品（如EC，纷享销客）
+ *
+ * @author wuweifeng wrote on 2018/1/18.
+ */
+@Entity
+@Table(name = "pt_menu", indexes = {@Index(name = "parent_id", columnList =
+        "parentId")})
+public class PtProduct extends BaseEntity {
+    /**
+     * 产品名字
+     */
+    private String name;
+    /**
+     * 当前状态（0正常，1内测，2异常）
+     */
+    private Integer state;
+    /**
+     * db描述，可能不同的产品将来连不同的库、表
+     */
+    private String db;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getDb() {
+        return db;
+    }
+
+    public void setDb(String db) {
+        this.db = db;
+    }
+
+    @Override
+    public String toString() {
+        return "PtProduct{" +
+                "name='" + name + '\'' +
+                ", state=" + state +
+                ", db='" + db + '\'' +
+                '}';
+    }
+}

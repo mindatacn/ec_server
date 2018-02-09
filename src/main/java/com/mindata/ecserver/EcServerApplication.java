89eaa99f2930a7c0c6e156447a2cc774856aa09e
wuwf <272551766@qq.com>
2018-02-09 17:46:02 +0800

    redis加锁时，设置过期时间

--- src/main/java/com/mindata/ecserver/EcServerApplication.java
+++ src/main/java/com/mindata/ecserver/EcServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * wolf
+ */
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
